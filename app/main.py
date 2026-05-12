9802948e8cd1266c314d7c082f08951433470b7d
yahadimon8-deep <yahadimon8@gmail.com>
2026-05-13 02:10:15 +0300

    solution: implement cache decorator

--- app/main.py
+++ app/main.py
@@ -1,6 +1,20 @@
+import functools
 from typing import Callable
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    cache_dict = {}
+
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        key = (args, tuple(sorted(kwargs.items())))
+        if key in cache_dict:
+            print("Getting from cache")
+            return cache_dict[key]
+        else:
+            print("Calculating new result")
+            result = func(*args, **kwargs)
+            cache_dict[key] = result
+            return result
+
+    return wrapper
